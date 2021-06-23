7cef461e1f1d5bc1325b3596e1489edf922f9887
N0mercy00 <dkdhehesp@naver.com>
2021-06-23 22:42:02 +0900

    useState, useRef 예제 실습

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,24 +1,59 @@
-import React,{useState} from 'react';
+import React,{useState, useRef} from 'react';
 
 function InputSample(){
-    //상태관리
-    const [text,setText]=useState('');
+    //useState과 객체를 관리하도록
+    const [inputs, setInputs]=useState({
+        name:'',
+        nickname:'',
+    });
     
+    //ref실습
+    const nameInput = useRef();
+
+    //비구조 할당으로 추출하기
+    const {name,nickname}=inputs;
+
     //입력값 반영
     const onChange=(e)=>{
-        setText(e.target.value);
-    }
+        //인풋박스에 입력된 내용과 인풋박스의 이름 추출
+        const {name,value}= e.target;
+
+        setInputs({
+            ...inputs,
+            [name]:value,
+        });
+    };
 
     //초기화 이벤트
     const onReset=()=>{
-        setText('');
-    }
+        setInputs({
+            name:'',
+            nickname:'',
+        });
+        nameInput.current.focus();
+ 
+    };
+    
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input 
+            name="name"
+            placeholder="name" 
+            onChange={onChange} 
+            value={name}  
+            ref={nameInput}
+            />
+
+            <input 
+            name="nickname" 
+            placeholder="nickname" 
+            onChange={onChange} 
+            value={nickname}
+            />
+
             <button onClick={onReset}>reset</button>
             <div>
-                <b>value : </b> {text}
+                <b>value : </b> {name} ({nickname})
             </div>
         </div>
     )
